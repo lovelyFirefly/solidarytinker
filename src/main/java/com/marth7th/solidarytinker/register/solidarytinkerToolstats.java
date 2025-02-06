7adea8158d827f21dd0940be4ea3e77f348b4491
lovelyFirefly <1494932845@qq.com>
2025-02-06 12:01:09 +0800

    小更改

--- src/main/java/com/marth7th/solidarytinker/register/solidarytinkerToolstats.java
+++ src/main/java/com/marth7th/solidarytinker/register/solidarytinkerToolstats.java
@@ -1,11 +1,12 @@
 package com.marth7th.solidarytinker.register;
 
 import com.marth7th.solidarytinker.solidarytinker;
+import slimeknights.tconstruct.library.modifiers.Modifier;
 import slimeknights.tconstruct.library.tools.stat.FloatToolStat;
 import slimeknights.tconstruct.library.tools.stat.ToolStatId;
 import slimeknights.tconstruct.library.tools.stat.ToolStats;
 
-public class solidarytinkerToolstats {
+public class solidarytinkerToolstats extends Modifier {
     public static final FloatToolStat ENERGY_STORE = ToolStats.register(new FloatToolStat(name("energy_capacity"), -16760319, 0.0F, 0.0F, Integer.MAX_VALUE));
     public static final FloatToolStat MANA_STORE = ToolStats.register(new FloatToolStat(name("mana_store"), -16760319, 0.0F, 0.0F, Integer.MAX_VALUE));
 
